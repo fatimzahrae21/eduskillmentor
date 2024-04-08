4863f671703349e6097db8549cb00f4e7c9f2e59
souad el maazouzi <souadmaazouzi1@gmail.com>
2024-04-08 03:44:19 +0000

    front end

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [indexController::class, 'index']);
+/*Route::get('/', [indexController::class, 'index']);*/
 Route::get('/', [homeController::class, 'index']);
 Route::get('/Formation', [formationController::class, 'index']);
