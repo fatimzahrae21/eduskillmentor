d7935f164d3232bc0747044bb3420e52a9904cee
souad el maazouzi <souadmaazouzi1@gmail.com>
2024-04-09 16:10:31 +0000

    add comment

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 /*Route::get('/', [indexController::class, 'index']);*/
+/*add*/
 Route::get('/', [homeController::class, 'index']);
 Route::get('/Formation', [formationController::class, 'index']);
