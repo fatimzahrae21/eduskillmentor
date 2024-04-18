8d813b6c2aafd76b2d106a64f7dfbaa7df3dba15
souad el maazouzi <souadmaazouzi1@gmail.com>
2024-04-18 02:01:02 +0100

    commit

--- resources/views/teams/team.blade.php
+++ resources/views/teams/team.blade.php
@@ -1,10 +1,16 @@
 
 <x-master title="infos page">
+           
+
+	             
 
 	
-<table id="example" class="table table-striped table-bordered" cellspacing="0" width="100%">
-	<button type="button" class="btn btn-primary">Ajouter</button> 
+	  <button id="openModalBtn"  class="btn btn-primary">Ajouter team</button>
+         
+				 <table id="example" class="table table-striped table-bordered" cellspacing="0" width="100%">
+	
 	<thead>
+		
 		<tr>
 			<th>nom</th>
 			<th>specialites</th>
@@ -38,8 +44,57 @@
 		
 	</tbody>
 </table>
+      
+<div id="myModal" class="modal">
 
-
+	<!-- Modal content -->
+	<div class="modal-content">
+	  <span class="close">&times;</span>
+	  <form>
+		<div class="form-group">
+		  
+			<label for="nom">NOM</label>
+			<input type="text" class="form-control" id="nom" placeholder="Email">
+		
+		 
+		</div>
+		
+		<div class="form-group">
+			<label class="form-label">
+				Spécialités</label>
+			<input type="text" name="image" class="form-control" >
+		</div>
+		<div class="form-group">
+			<label class="form-label">Image de formateur</label>
+			<input type="file" name="image" class="form-control" >
+		</div>
+		<div class="form-row">
+		  <div class="form-group col-md-4">
+			<label for="facebook"><a href="#"><i class="bi bi-facebook"></i></a> lien facebook</label>
+			<input type="text" class="form-control" id="inputCity">
+		  </div>
+		  <div class="form-group col-md-2">
+			<label for="twitterlink"> 	<a href="#"><i class="bi bi-twitter"></i></a> lien twitter</label>
+			<input type="text" class="form-control" id="twitterlink">
+		  </div>
+		  <div class="form-group col-md-4">
+			<label for="linkinsta"><a href="#"><i class="bi bi-instagram"></i></a> lien instagram</label>
+			<input type="text" class="form-control" id="linkinsta">
+		  </div>
+		  <div class="form-group col-md-2">
+			<label for="linkdin">
+				<a href="#"><i class="bi bi-linkedin"></i></a> lien linkdin</label>
+			<input type="text" class="form-control" id="linkdin">
+		  </div>
+		</div>
+		
+		
+		
+		<button type="submit" class="btn btn-primary">Ajouter</button>
+	  </form>
+	</div>
+  
+  </div>
 
 
 </x-master>
