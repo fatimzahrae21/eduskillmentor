e0348222b2caacb16cd4f74c73cfc769b3026936
fatimzahrae21 <fatimajdidi18@gmail.com>
2024-04-19 16:49:20 +0100

    message

--- resources/views/Formation-details.blade.php
+++ resources/views/Formation-details.blade.php
@@ -68,9 +68,9 @@
           <li><a href="#hero">Home</a></li>
           <li><a href="#about">About</a></li>
           <li><a href="#services">Services</a></li>
-          <li><a href="#Formation">Our formations</a></li>
+          <li><a href="/Formation">Our formations</a></li>
           <li><a href="#team">Team</a></li>
-          <li><a href="/#recent-posts">News</a></li>
+          <li><a href="#recent-posts">News</a></li>
 
           <li><a href="#contact">Contact</a></li>
         </ul>
